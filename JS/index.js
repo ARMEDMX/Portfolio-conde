80d8d4eabeafa1803dcdf99db6a7b7188bfe93d8
Conde <frontdevconde@gmail.com>
2025-04-16 01:40:37 -0600

    Merged dev into main new modified

--- JS/index.js
+++ JS/index.js
@@ -1,16 +1,20 @@
+let prevScrollPos = window.pageYOffset;
+const navbar = document.querySelector(".navbar");
 
-    let prevScrollPos = window.pageYOffset;
-    const navbar = document.querySelector(".navbar");
+window.addEventListener("scroll", () => {
+    const currentScrollPos = window.pageYOffset;
 
-    window.addEventListener("scroll", () => {
-        const currentScrollPos = window.pageYOffset;
-
-        if (prevScrollPos > currentScrollPos) {
-            navbar.classList.remove("hidden"); // Scroll hacia arriba -> mostrar navbar
-        } else {
-            navbar.classList.add("hidden"); // Scroll hacia abajo -> ocultar navbar
-        }
-
-        prevScrollPos = currentScrollPos;
-    });
-
+    // Si el scroll está en la parte superior, aseguramos que el navbar se vea
+    if (currentScrollPos === 0) {
+        navbar.classList.remove("hidden");
+    }
+    // Si el usuario ha subido (se detecta una disminución de scroll)
+    else if (prevScrollPos > currentScrollPos) {
+        navbar.classList.remove("hidden");
+    } else {
+        // Si el usuario está bajando, se oculta el navbar
+        navbar.classList.add("hidden");
+    }
+    
+    prevScrollPos = currentScrollPos;
+});
--- JS/scroll.js
+++ JS/scroll.js
@@ -1,16 +1,20 @@
 // JavaScript para agregar clase 'visible' cuando la sección entra en la vista
 document.addEventListener("DOMContentLoaded", function () {
+    // Selecciona todas las secciones que deseas observar
     const sections = document.querySelectorAll('.fadeInSection');
     
+    // Crea un nuevo IntersectionObserver
     const observer = new IntersectionObserver((entries, observer) => {
         entries.forEach(entry => {
+            // Si la sección es visible en la pantalla
             if (entry.isIntersecting) {
-                entry.target.classList.add('visible');
-                observer.unobserve(entry.target);
+                entry.target.classList.add('visible'); // Agrega la clase 'visible'
+                observer.unobserve(entry.target); // Deja de observar la sección
             }
         });
-    }, { threshold: 0.5 }); // La sección se anima cuando el 50% está en la vista
+    }, { threshold: 0.1 }); // Activar cuando el 10% de la sección esté en la vista
 
+    // Observa cada sección
     sections.forEach(section => {
         observer.observe(section);
     });
